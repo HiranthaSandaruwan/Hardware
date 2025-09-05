9fb21ccb9dcb1aa46eaf7ccd28e319135d62f739
DulhanHansaja <dulhanugc@gmail.com>
2025-09-05 18:04:48 +0530

    updated

--- help.php
+++ help.php
@@ -1,7 +1,7 @@
 <?php include __DIR__ . '/partials/header.php'; ?>
   <style>
   .help {
-      margin-top: 2.5rem; 
+      margin-top: 2rem; 
       margin-bottom: 2rem;
       padding: 2rem;
       border-radius: 12px;
